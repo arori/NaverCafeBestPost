1108544e015b55fcbda9d2af4c7fe3b4fa9bac77
Heejung Park <cho_hol@outlook.com>
2019-08-14 17:48:23 +0900

    리팩토링

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
 (function () {
-  var CAFE_REG_EXP = /^https?:\/\/cafe\.naver\.com\/.+[^?]/;
-
   function checkForValidUrl(tabId, changeInfo, tab) {
-    if (CAFE_REG_EXP.test(tab.url)) {
+    if (/^https?:\/\/cafe\.naver\.com\/.+[^?]/.test(tab.url)) {
       chrome.pageAction.show(tabId);
     }
   };
 
-  chrome.tabs.onUpdated.addListener(checkForValidUrl);
-  
-  chrome.pageAction.onClicked.addListener(function (tab) {
+  function redirectBestPost(tab) {
     var extractedUrl = tab.url.split("?")[0];
-    chrome.tabs.update(tab.tabId, { url: extractedUrl + '?iframe_url=/BestArticleList.nhn' });
-  });
+    chrome.tabs.update(tab.tabId,
+      { url: extractedUrl + '?iframe_url=/BestArticleList.nhn' });
+  }
+
+  chrome.tabs.onUpdated.addListener(checkForValidUrl);
+  chrome.pageAction.onClicked.addListener(redirectBestPost);
 })();
